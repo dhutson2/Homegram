3c4bb8d541645e5566fdc8b90baf9db007005045
Derek Hutson <denverd1301@gmail.com>
2019-08-26 10:28:05 -0600

    kjaskjsa

--- config/database.php
+++ config/database.php
@@ -1,9 +1,11 @@
 <?php
 
-$DATABASE_URL=parse_url('postgres://oybpovorcavrmx:977c44dd6bf14107dc289d8995f2599aa7e44c59809288ce43153120a18352ea@ec2-23-23-182-18.compute-1.amazonaws.com:5432/dcmgetsjclr0go
-');
+
+
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://oybpovorcavrmx:977c44dd6bf14107dc289d8995f2599aa7e44c59809288ce43153120a18352ea@ec2-23-23-182-18.compute-1.amazonaws.com:5432/dcmgetsjclr0go
+');
 return [
 
     /*
@@ -76,7 +78,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
